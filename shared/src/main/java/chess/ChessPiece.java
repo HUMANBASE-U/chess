dae21814c6c8bfcb2958968c2d32cc439f524e7e
HUMANBASE-U <yaodadahang@163.com>
2026-01-21 19:29:46 -0700

    Add initial move validation logic to ChessPiece
    Introduced a draft of the inside_checker method for move validation, including boundary and team checks. Added placeholders for diagonal move generation and started refactoring pieceMoves to support more detailed move logic.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,7 +1,8 @@
 package chess;
-
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
 
 /**
  * Represents a single chess piece
@@ -54,8 +55,45 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
+
+
+
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
+
+        List<String> list_diagonal = new ArrayList<>();
+        boolean inside_checker(piece.myPosition){
+            //this inside_checker will kill all invalid moves before it slip in to move_lists.
+
+        //boundary check
+            if (myPosition.getRow()>=9  |  myPosition.getColumn()>=9 |
+                myPosition.getRow()<=0  |  myPosition.getColumn()>=9 ){
+                return false;
+            }
+
+        //blocked by enemy (arrest me after you add!)
+            if (board.getPiece(myPosition)!= null && piece.getTeamColor()!=this.getTeamColor()){
+                return false;
+            };
+
+        //BLOCKED BY ALLEY (arrest me before you add!)
+            if (board.getPiece(myPosition)!= null && piece.getTeamColor()!=this.getTeamColor()){
+                return false;
+            };
+
+        }
+
+        for (int i=0; i<8; i++){
+
+            for (int j=0; j<8; j++){
+
+                if ChessPiece(i,j)
+            }
+
+        }
+
+
         if(piece.getPieceType() == PieceType.BISHOP){
             return   List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8),null));
         }
