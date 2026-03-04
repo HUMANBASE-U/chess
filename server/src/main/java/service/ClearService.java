41ef30fd3f1c3e1cb2eb1ed316de9f29b1f7c7f2
HUMANBASE-U <yaodadahang@163.com>
2026-03-03 22:59:07 -0700

    6

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,16 @@
 package service;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+
 public class ClearService {
+    private final DataAccess dao;
+
+    public ClearService(DataAccess dao) {
+        this.dao = dao;
+    }
+
+    public void deleteData() throws DataAccessException {
+        dao.clear();
+    }
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -9,33 +9,49 @@ import java.util.UUID;
 
 public class UserService {
 
-    private DataAccess dao;
+    private final DataAccess dao;
 
     public UserService(DataAccess dao) {
         this.dao = dao;
     }
 
-    public RR.RegisterResult register(RR.RegisterRequest registerRequest) throws DataAccessException, AlreadyTakenException {
-          if(dao.getUser(registerRequest.username())!=null) throw new AlreadyTakenException("Error: already taken");
+    public RR.RegisterResult register(RR.RegisterRequest registerRequest) throws DataAccessException, AlreadyTakenException, BadRequestException {
+        verifyInCaseBlank(registerRequest.username(), registerRequest.password(), registerRequest.email());  //Pre
 
-
+        if(dao.getUser(registerRequest.username())!=null) throw new AlreadyTakenException("Error: already taken");
 
 //if nothing wrong, do:
-          String authToken = newAuth();
-          dao.createUser(new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email())); // 存入服务器userData
-          dao.createAuth(new AuthData(authToken, registerRequest.username()));//服务器存入authdata
+        String authToken = newAuth();
+        dao.createUser(new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email())); // 存入服务器userData
+        dao.createAuth(new AuthData(authToken, registerRequest.username()));//服务器存入authdata
         return new RR.RegisterResult(registerRequest.username(), authToken);
     }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public RR.LoginResult login(RR.LoginRequest loginRequest) throws DataAccessException{
-        return
+
+        String authToken = newAuth();
+        return new RR.LoginResult(loginRequest.username(), authToken);
     }
+
+
     public void logout(RR.LogoutRequest logoutRequest) throws DataAccessException{}
+
+    /// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
     private static String newAuth() {
         return UUID.randomUUID().toString();
     }
 
+    private static void verifyInCaseBlank(String... lines) throws BadRequestException{
+        int count = 0,num = 0;
+        for(String l : lines){
+            num++;
+            if(l!=null) count++;
+        }
+        if(count != num) throw new BadRequestException("Error: bad request");
+    }
 }
