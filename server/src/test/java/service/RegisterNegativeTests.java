50bc589f2ddd5bafe2defd763afc8c90f62446d1
HUMANBASE-U <yaodadahang@163.com>
2026-03-05 20:23:46 -0700

    18

--- server/src/test/java/service/RegisterNegativeTests.java
+++ server/src/test/java/service/RegisterNegativeTests.java
@@ -1,4 +1,30 @@
 package service;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class RegisterNegativeTests {
+    private DataAccess dao;
+    private UserService userService;
+
+    @BeforeEach
+    void setUp(){
+        dao = new MemoryDao();
+        userService = new UserService(dao);
+    }
+
+    @Test//negative need
+    @DisplayName("wrong input , test for the BadRequestException")
+
+    void setRegisterservice(){
+        assertThrows(BadRequestException.class,
+        () ->  userService.register(new RR.RegisterRequest(null, null, "S"))
+        );
+    }
 }
--- server/src/test/java/service/RegisterPositiveTests.java
+++ server/src/test/java/service/RegisterPositiveTests.java
@@ -1,4 +1,32 @@
 package service;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class RegisterPositiveTests {
+    private DataAccess dao;
+    private UserService userService;
+
+    @BeforeEach
+    void setUp(){
+        dao = new MemoryDao();
+        userService = new UserService(dao);
+    }
+
+    @Test
+    @DisplayName("Normal register process")
+    void setRegisterservice() throws BadRequestException, DataAccessException, AlreadyTakenException {
+        RR.RegisterResult result = userService.register(new RR.RegisterRequest("s", "s", "s"));
+
+        assertNotNull(result.authToken());
+        assertEquals("s", dao.getUser("s").password());
+    }
+
 }
