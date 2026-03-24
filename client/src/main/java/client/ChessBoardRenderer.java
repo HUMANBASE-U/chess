4b09e91d5aa9ad963e411a6c2961ca950dd78158
HUMANBASE-U <yaodadahang@163.com>
2026-03-23 22:18:56 -0600

    8 bugs

--- client/src/main/java/client/ChessBoardRenderer.java
+++ client/src/main/java/client/ChessBoardRenderer.java
@@ -1,19 +1,60 @@
 package client;
 
+import chess.ChessBoard;
+import chess.ChessGame;
 import chess.ChessPiece;
+import chess.ChessPosition;
 import ui.EscapeSequences;
 
-public class ChessBoardRenderer {
-    private ChessPiece[][] squares;
+import static ui.EscapeSequences.*;
 
-    public ChessBoardRenderer(ChessPiece[][] squares) {
-        this.squares = squares;
+public class ChessBoardRenderer {
+    private ChessBoard board;
+    private ChessGame.TeamColor color;
+    private boolean IS_WHITE;
+    private boolean colorFliper;
+
+    public ChessBoardRenderer(ChessBoard board, ChessGame.TeamColor color, boolean IS_WHITE, boolean colorFliper) {
+        this.board = board;
+        this.color = color;
+        this.IS_WHITE = (color == ChessGame.TeamColor.WHITE);
+        this.colorFliper = IS_WHITE? true : false;
     }
 
-    public void drawBoard{
-        System.out.print(EscapeSequences.ERASE_SCREEN);
+    public void drawBoard(){
+        System.out.print(ERASE_SCREEN);
+        //如果是黑色的，r,c全都
+        for(int r=1; r<=8; r++){
+            for(int c=1; c<=8; c++){
+                //双层for循环意味着每个方块打印一个 bg + target + reset + reset txt
+                //未完成：标尺
 
-        
+                ChessPiece target = board.getPiece(new ChessPosition(r, c));
+                //如果是白的，那第一个绘制的left bottom背景是深色的，这样白棋的queen正好是light色
+                String bg = colorFliper? SET_BG_COLOR_DARK_GREEN : SET_BG_COLOR_LIGHT_GREY;
+                String p = setPiece(target);
+                colorFliper = !colorFliper;
+
+                System.out.print(bg + p + RESET_BG_COLOR + RESET_TEXT_COLOR);
+
+            }
+            colorFliper = !colorFliper;//行与行之间会有两个块同色，这里修复了这个bug
+            System.out.println(); //换行
+        }
+    }
+
+    private String setPiece(ChessPiece target) {
+        if (target == null) return EMPTY;
+        boolean IS_TARGET_WHITE = target.getTeamColor() == ChessGame.TeamColor.WHITE;
+
+        return switch (target.getPieceType()){
+            case KING -> IS_TARGET_WHITE? WHITE_KING : BLACK_KING;
+            case QUEEN -> IS_TARGET_WHITE? WHITE_QUEEN : BLACK_QUEEN;
+            case BISHOP -> IS_TARGET_WHITE? WHITE_BISHOP : BLACK_BISHOP;
+            case KNIGHT -> IS_TARGET_WHITE? WHITE_KNIGHT : BLACK_KNIGHT;
+            case ROOK -> IS_TARGET_WHITE? WHITE_ROOK : BLACK_ROOK;
+            case PAWN -> IS_TARGET_WHITE? WHITE_PAWN : BLACK_PAWN;
+        };
     }
 
 
